cba2c317062188f20f762ec78ea7d5c872dc5f02
Colin <colin_teahan@yahoo.com>
2025-04-24 09:13:26 -0700

    added another function overload

--- src/try.ts
+++ src/try.ts
@@ -3,12 +3,14 @@
 
 // Result tuple which contains a value or an error
 export type Result<T> = [T, undefined] | [undefined, Error]
+export type IsAsync<F> = F extends (...args: any[]) => Promise<any> ? true : false
 
 export class Try {
     static err = (e: unknown) => e instanceof Error ? e : new Error(String(e))
 
     static _handler<T, Fn extends () => T>(fn: Fn, isAsync?: false): Result<T>
-    static _handler<T, Fn extends () => T>(fn: Fn, isAsync?: true): Promise<Result<T>>
+    static _handler<T, Fn extends () => Promise<T>>(fn: Fn, isAsync?: true): Promise<Result<T>>
+    static _handler<T, Fn extends () => T>(fn: Fn, isAsync?: true): Promise<Result<T>> | Promise<Result<Promise<T>>>
     static _handler<T, Fn extends () => T>(fn: Fn, isAsync?: boolean): Result<T> | Promise<Result<T>> {
         try {
             const output = fn()
@@ -29,9 +31,24 @@ export class Try {
         }
     }
 
-    static catch<T>(fn: () => Promise<T>): Promise<[T, undefined]> | Promise<[undefined, Error]>
-    static catch<T>(fn: () => T): [T, undefined] | [undefined, Error]
-    static catch<T>(fn: () => T | Promise<T>) {
+    /**
+     * @note the ordering of catch overloads matters and the Promise<Result<T>>
+     * overload needs to appear first (possibly reverse of the _handler).
+     * This can cause some issues if the only return type is never.
+     * 
+     * ```ts
+     * const [_, err] = Try.catch(() => {
+     *    throw new Error('Edge case')
+     *    // un-comment the next line and it works?
+     *    // return 123; 
+     * }) 
+     * ```
+     */
+
+
+    static catch<T>(fn: () => Promise<T>): Promise<Result<T>>
+    static catch<T>(fn: () => T): Result<T>
+    static catch<T>(fn: () => T | Promise<T>): Result<T> | Promise<Result<T>> {
         try {
             const value = fn()
             if (value instanceof Promise) {
---
+++ test/index.test.ts
@@ -0,0 +1,35 @@
+import { test, expect } from "bun:test";
+import { Try } from "../index";
+
+test("Try.catch can run synchronous function", () => {
+  const [value, error] = Try.catch(() => 123)
+  expect(value).toBe(123);
+  expect(error).toBeUndefined()
+});
+
+test("Try.catch can catch synchronous errors", () => {
+  const [value, error] = Try.catch(() => {
+    throw new Error("test")
+    return 123
+  })
+  expect(value).toBeUndefined()
+  expect(error).toBeDefined()
+  expect(error?.message).toBe("test")
+});
+
+test("Try.catch can run asynchronous function", async () => {
+  const [value, error] = await Try.catch(async () => {
+    return 456
+  })
+  expect(value).toBe(456);
+  expect(error).toBeUndefined()
+});
+
+test("Try.catch can catch asynchronous errors", async () => {
+  const [value, error] = await Try.catch(() => {
+    throw new Error("test")
+  })
+  expect(value).toBeUndefined()
+  expect(error).toBeDefined()
+  expect(error?.message).toBe("test")
+});
