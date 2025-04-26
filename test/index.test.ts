d3cdfd40e9b39f9b7fa469acd75091fa601b9b11
Colin <colin_teahan@yahoo.com>
2025-04-25 22:19:05 -0700

    updated with the result class

--- test/index.test.ts
+++ test/index.test.ts
@@ -83,8 +83,36 @@ test('Result.ok method is true when value is present', () => {
   }
 })
 
+test('Result.ok method is true when value is present (async)', async () => {
+  const result = await Try.catch(async () => ({
+    data: 'hello',
+  }))
+  expect(result.ok).toBe(true)
+  expect(result[0]).toBeDefined()
+  expect(result[1]).toBeUndefined()
+  expect(result.value).toBeDefined()
+  expect(result.error).toBeUndefined()
+  if (result.ok) {
+    expect(result.value.data).toEqual('hello')
+  }
+})
+
 test('Result.ok method is false when error is present', () => {
   const result = Try.catch(() => {
+    throw new Error('failed')
+  })
+  expect(result.ok).toBe(false)
+  expect(result[0]).toBeUndefined()
+  expect(result[1]).toBeDefined()
+  expect(result.value).toBeUndefined()
+  expect(result.error).toBeDefined()
+  if (!result.ok) {
+    expect(result.error.message).toBe('failed')
+  }
+})
+
+test('Result.ok method is false when error is present (async)', async () => {
+  const result = await Try.catch(async () => {
     throw new Error('failed')
   })
   expect(result.ok).toBe(false)
@@ -110,11 +138,25 @@ test('Result.unwrap returns value or throws', () => {
 
 test('Result.unwrapOr returns value or fallback', () => {
   const result1 = Try.catch(() => {
-    return 'or1'
+    return 123
   })
-  expect(result1.unwrapOr('fallback1')).toBe('or1')
+  expect(result1.unwrapOr('fallback1')).toBe(123)
   const result2 = Try.catch(() => {
     throw new Error('unwrap')
   })
   expect(result2.unwrapOr('fallback2')).toBe('fallback2')
+})
+
+test('Edge case where result is error', () => {
+  const result = Try.catch(() => new Error('errorAsValue'))
+  expect(result.ok).toBeTrue()
+  expect(result.value?.message).toBe('errorAsValue')
+  expect(result[0]?.message).toBe('errorAsValue')
+  expect(result.unwrapOr(new Error('other'))?.message).toBe('errorAsValue')
+})
+
+test('Edge case where function never returns', () => {
+  const result = Try.catch(() => {})
+  expect(result.ok).toBeTrue()
+  expect(result.value).toBeUndefined()
 })
